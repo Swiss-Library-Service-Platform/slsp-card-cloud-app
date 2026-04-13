9f3f7fc8cc118aa0c887fb614d5c2c370846abe2
v1lling <sascha.villing@web.de>
2026-04-13 10:04:16 +0200

    Refactor library card number regex patterns for consistency and clarity

--- cloudapp/src/app/model/librarycardnumber.model.ts
+++ cloudapp/src/app/model/librarycardnumber.model.ts
@@ -9,58 +9,72 @@ export class Librarycardnumber {
 
     static allowedRegex = [
         // RERO Freiburg
-        /^(200)\d{7}$/,
+        /^200\d{7}$/i,
         // RERO Valais
-        /201\d{7}/,
+        /^201\d{7}$/i,
         // RERO Neuchâtel
-        /20[345]\d{7}/,
+        /^20[345]\d{7}$/i,
         // RERO Genève
-        /(20[67]\d{7}|307\d{7}|407\d{7}|507\d{7})/,
+        /^(20[67]\d{7}|307\d{7}|407\d{7}|507\d{7})$/i,
         // Bundesinstitutionen
-        /208\d{7}/,
+        /^208\d{7}$/i,
         // Service Bibliothèques & Archives de la Ville de Lausanne
-        /BVL\d{7}/,
+        /^BVL\d{7}$/i,
         // RERO Vaud
-        /209\d{7}/,
+        /^209\d{7}$/i,
         // Olympic World Library (Le Centre d’Etudes Olympiques)
-        /220\d{7}/,
+        /^220\d{7}$/i,
         // RERO IUED
-        /(L[1-9]\d{2}|L\d{4})/,
+        /^(L[1-9]\d{2}|L\d{4})$/i,
         // Alexandria
-        /295\d{7}/,
+        /^295\d{7}$/i,
         // Schweizerische Nationalbibliothek
-        /(nb\d+|210\d{7})/,
+        /^(nb\d+|210\d{7})$/i,
+        /^290\d{6,7}$/i,
         // IDS Basel
-        /[AB][A-Z\d]*\d{2,}[A-Z\d]*/, /(A[A-Z\d]*\d{2,}[A-Z\d]*|MBS[A-Z\d]*\d{2,}[A-Z\d]*|\d{7}|\d{11})/,
+        /^[AB][A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(A[A-Z\d]*\d{2,}[A-Z\d]*|MBS[A-Z\d]*\d{2,}[A-Z\d]*|\d{7}|\d{11})$/i,
         // IDS Bern
-        /B[A-Z\d]*\d{2,}[A-Z\d]*/, /(B[A-Z\d]*\d{2,}[A-Z\d]*|\d{9})/,
+        /^B[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(B[A-Z\d]*\d{2,}[A-Z\d]*|\d{9})$/i,
         // IDS Luzern
-        /L\-[A-Z\d]*\d{2,}[A-Z\d]*/, /(L[A-Z\d]*\d{2,}[A-Z\d]*|FHZ\d{7})/,
+        /^L\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(L[A-Z\d]*\d{2,}[A-Z\d]*|FHZ\d{7})$/i,
         // IDS St. Gallen
-        /H\-[A-Z\d]*\d{2,}[A-Z\d]*/, /(H[A-Z\d]*\d{2,}[A-Z\d]*|\d{13})/,
+        /^H\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(H[A-Z\d]*\d{2,}[A-Z\d]*|\d{13})$/i,
         // IDS Zürich Universität
-        /U\-[A-Z\d]*\d{2,}[A-Z\d]*/, /([UM][A-Z\d]*\d{2,}[A-Z\d]*)/,
+        /^U\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^([UM][A-Z\d]*\d{2,}[A-Z\d]*)$/i,
         // NEBIS
-        /E\-[A-Z\d]*\d{2,}[A-Z\d]*/, /([ESP][A-Z\d]*\d{2,}[A-Z\d]*)/,
+        // This regix unfortunately matches a lot of numbers
+        /^E\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^([ESP][A-Z\d]*\d{2,}[A-Z\d]*)$/i,
         // IDS Zürich Zentralbibliothek
-        /(Z\-[A-Z\d]*\d{2,}[A-Z\d]*|E\-[A-Z\d]*\d{2,}[A-Z\d]*)/, /[ZY][A-Z\d]*\d{2,}[A-Z\d]*/,
+        /^(Z\-[A-Z\d]*\d{2,}[A-Z\d]*|E\-[A-Z\d]*\d{2,}[A-Z\d]*)$/i, /^[ZY][A-Z\d]*\d{2,}[A-Z\d]*$/i,
         // Aargauer Bibliotheksnetz
-        /(ABN\d+|BBM\d+)/,
+        /^(ABN\d+|BBM\d+)$/i,
         // Bibliotheksverbund Graubünden
-        /Q[A-Z\d]*\d{2,}[A-Z\d]*/, /(Q\d+|\d{9})/,
+        /^Q[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(Q\d+|\d{9})$/i,
         // St. Galler Bibliotheksnetz (SGBN)
-        /G\-[A-Z\d]*\d{2,}[A-Z\d]*/, /(G\d+|GKBG\d+|GKSB\d+|GKSH\d+|GKWA\d+|GKWI\d+|GBAR\d+|GBEB\d+)/,
+        /^G\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(G\d+|GKBG\d+|GKSB\d+|GKSH\d+|GKWA\d+|GKWI\d+|GBAR\d+|GBEB\d+|GKSS\d+)$/i,
         // Sistema Bibliotecario Ticinese (SBT)
-        /T\-[A-Z\d]*\d{2,}[A-Z\d]*/, /(T[A-Z\d]*\d{2,}[A-Z\d]*|202\d{7})/,
+        /^T\-[A-Z\d]*\d{2,}[A-Z\d]*$/i, /^(T[A-Z\d]*\d{2,}[A-Z\d]*|202\d{7})$/i,
         // HSG staff library card numbers. See https://task.slsp.ch/browse/SUPPORT-4486
-        /62991200\d{8}/,
+        /^62991200\d{8}$/i,
         // FHNW library card numbers. See https://task.slsp.ch/browse/SUPPORT-7471
-        /N\d{8,9}/,
+        /^N\d{8,9}$/i,
         // OST library card numbers. See https://task.slsp.ch/browse/SUPPORT-7935
-        /OST\d+/,
+        /^OST\d+$/i,
+        // Pädagogische Hochschule Zürich, Bibliothek
+        /^PHZH\d{6}$/i,
+        // USI library. See https://slsp.atlassian.net/browse/SUPPORT-15500
+        /^19179\d{5}$/i,
+        // HESSO Library. See https://slsp.atlassian.net/browse/SUPPORT-18045
+        /^HES\d{7}$/i,
+        // Berner Bildungszentrum Pflege. See https://slsp.atlassian.net/browse/SUPPORT-19620
+        /^BZP\d{7}$/i,
+        // Israelitisch Cultusgemeinde Zürich. See https://slsp.atlassian.net/browse/SUPPORT-16764
+        /^ICZ\d{7}$/i,
+        // Landesbibliothek Liechtenstein LiLB / Bibliothek Universität Liechtenstein
+        /^FM\d{6}$/i, /^FS\d{6}$/i, /^C\d{6}$/i, /^ILL\d{4}$/i, /^GBA[LS]?\d+$/i, /^GRU\d{5,6}$/i, /^GSB\d{5,6}$/i, /^SZM\d{5,6}$/i, /^SZU[A-Z\d]?\d{5}$/i, /^IAP\d{5}$/i,
         // SLSP
-        /SLSP\-\d{9}$/, // Altes Format
-        /SLSP\d{9}/ // Neues Format
+        /^SLSP\-\d{9}$/i, // Altes Format
+        /^SLSP\d{9}$/i // Neues Format
     ];
 
     /**
@@ -144,15 +158,7 @@ export class Librarycardnumber {
         }
 
         // Check all regex of possible numbers from IZs
-        // TODO: check all regex
-        let isMatched = false;
-        this.allowedRegex.forEach((regex) => {
-            var regExp = new RegExp(regex);
-            if (librarycardnumber.match(regExp)) {
-                isMatched = true;
-            }
-        });
-        return isMatched;
+        return this.allowedRegex.some(regex => librarycardnumber.match(regex));
     }
 
 
